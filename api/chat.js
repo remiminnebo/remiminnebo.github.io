82765f1098220bbf8b17f39f6d1110d6a9890982
Remi J Minnebo <remi.minnebo@alterdomus.com>
2025-09-03 16:21:17 +0200

    test

---
+++ api/chat.js
@@ -0,0 +1,17 @@
+export default function handler(req, res) {
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method === 'POST') {
+    const { message } = req.body;
+    return res.json({ response: `Echo: ${message}` });
+  }
+
+  res.status(405).json({ error: 'Method not allowed' });
+}
