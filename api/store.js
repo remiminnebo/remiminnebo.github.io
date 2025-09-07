0a032d8bfac0ae4c5c51640e164c12e9f502a669
Remi J Minnebo <remi.minnebo@alterdomus.com>
2025-09-07 22:31:29 +0200

    What OPTIONS do I have

--- api/store.js
+++ api/store.js
@@ -13,8 +13,14 @@ function verifySignature(data, signature) {
 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Max-Age', '86400');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
 
   if (req.method === 'POST') {
     const { question, answer } = req.body;
